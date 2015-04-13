e00446c6fcbea77b61847f0182d2818d5048ab71
Patrik Dufresne <ikus060@gmail.com>
2015-04-13 14:55:14 -0400

    Working version of account_invoice_repport

---
+++ pdsl_invoice/__openerp__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2012 Therp BV (<http://therp.nl>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    "name": "PDSL Invoices",
+    "version": "0.1",
+    "author": "Patrik Dufresne",
+    "category": 'Tools',
+    "description": """
+
+This module provide a custom invoice model.
+
+This module is compatible with OpenERP 7.0.
+
+""",
+    "depends": [
+        'account',
+        ],
+    "data": [
+        'pdsl_invoice_data.xml'
+        ],
+    'installable': True,
+    'active': False
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
