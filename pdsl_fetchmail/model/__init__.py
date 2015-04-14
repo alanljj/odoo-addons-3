e1b485d570b533f723dfc60ac09c13cf38dbf9f1
Patrik Dufresne <ikus060@gmail.com>
2015-04-14 15:32:07 -0400

    Add copyright statements.

--- pdsl_fetchmail/model/__init__.py
+++ pdsl_fetchmail/model/__init__.py
@@ -1,1 +1,9 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# PDSL fetchmail addons
+# Copyright(C) 2015 Patrik Dufresne Service Logiciel (http://www.patrikdufresne.com).
+#
+##############################################################################
+
 import pdsl_fetchmail
