ce9a82ebb49063baf967ea0b30be0b737d7f85d5
ikus060 <ikus060@ikus060-gb.patrikdufresne.com>
2017-09-02 18:46:40 -0400

    Add plugin for project sprint

---
+++ project_sprint/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright(C) 2017 Patrik Dufresne Service Logiciel inc. (http://www.patrikdufresne.com).
+{
+    "name": "Project Sprint",
+    "version": "8.0.1",
+    "author": "Patrik Dufresne Service Logiciel inc.",
+    "website": "http://www.patrikdufresne.com/",
+    "license": "AGPL-3",
+    "category": "Project",
+    "description": """
+Provide Sprint kanban view to Project module to allow planning of task using Agile sprint concept.
+""",
+    "depends": [
+        "project",
+    ],
+    "demo": [],
+    "data": [
+        "security/security_project_sprint.xml",
+        "security/ir.model.access.csv",
+        "view/project_sprint_view.xml",
+        "view/project_view.xml"
+    ],
+    "installable": True,
+    "active": False
+}
