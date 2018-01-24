dc7478105bc5e1867373e45a25cd61eb58d8fbb9
Patrik Dufresne <info@patrikdufresne.com>
2018-01-24 09:09:59 -0500

    hr_timesheet_x: Pre-filter the "responsible" with a tag. TASK-954

--- hr_timesheet_project_sheet/__manifest__.py
+++ hr_timesheet_project_sheet/__manifest__.py
@@ -38,6 +38,7 @@
         "report/report_timesheet_templates.xml",
         'data/hr_timesheet_project_sheet_data.xml',
         'data/hr_timesheet_action_data.xml',
+        'data/hr_timesheet_employee_tag.xml',
         'views/hr_analytic_timesheet.xml',
         'views/hr_timesheet_project_sheet_templates.xml',
         'views/hr_timesheet_project_sheet_views.xml',
